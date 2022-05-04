1c64d098b951b29af2fb2e466b64244529d93b31
Rodrigo Mukudai <mkdigo@gmail.com>
2022-05-04 21:28:43 +0900

    Add express and prisma

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,11 @@
-console.log('server');
+import express from 'express';
+
+const app = express();
+
+app.get('/users', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(3333, () => {
+  console.log('HTTP server running on port 3333!');
+});
