78a0af63e42a350e0ad32c1d3aaef23210c3ea8c
Rodrigo Mukudai <mkdigo@gmail.com>
2022-05-04 21:50:28 +0900

    Create Feedback

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,24 @@
 import express from 'express';
+import { prisma } from './prisma';
 
 const app = express();
 
-app.get('/users', (req, res) => {
-  res.send('Hello World');
+app.use(express.json());
+
+app.post('/feedbacks', async (req, res) => {
+  const { type, comment, screenshot } = req.body;
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot,
+    },
+  });
+
+  res.status(201).json({
+    data: feedback,
+  });
 });
 
 app.listen(3333, () => {
