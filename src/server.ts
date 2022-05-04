023c2f23e1491d07df5069bc7592146ef6bdc0e9
Rodrigo Mukudai <mkdigo@gmail.com>
2022-05-04 22:15:20 +0900

    Add nodemailer

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
 import express from 'express';
 import { prisma } from './prisma';
+import nodemailer from 'nodemailer';
 
 const app = express();
 
 app.use(express.json());
+
+const transport = nodemailer.createTransport({
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: 'dd2043dc6f73ce',
+    pass: '102ff9a097f21f',
+  },
+});
 
 app.post('/feedbacks', async (req, res) => {
   const { type, comment, screenshot } = req.body;
@@ -14,6 +24,18 @@ app.post('/feedbacks', async (req, res) => {
       comment,
       screenshot,
     },
+  });
+
+  await transport.sendMail({
+    from: 'Equipe Feedget <oi@feedget.com>',
+    to: 'Rodrigo Mukudai <mkdigo@gmail.com>',
+    subject: 'Novo feedback',
+    html: [
+      `<div style='font-family: sans-serif; font-size: 16px; color: #111;'>`,
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      `</div>`,
+    ].join('\n'),
   });
 
   res.status(201).json({
