3883a6e73b92cedb8f8cc8f1ca62236fce46864e
Rodrigo Mukudai <mkdigo@gmail.com>
2022-05-04 23:44:05 +0900

    Add Unit Test

--- src/use-cases/submit-feedback-use-case.ts
+++ src/use-cases/submit-feedback-use-case.ts
@@ -14,14 +14,23 @@ export class SubmitFeedbackUseCase {
   ) {}
 
   async execute(request: SubmitFeedbackUseCaseRequest) {
+    const { type, comment, screenshot } = request;
+
+    if (!type) throw new Error('Type is required.');
+    if (!comment) throw new Error('Type is required.');
+
+    if (screenshot && !screenshot.startsWith('data:image/png;base64')) {
+      throw new Error('Invalid screenshot format.');
+    }
+
     await this.feedbacksRepository.create(request);
 
     await this.mailAdapter.sendMail({
       subject: 'Novo Feedback',
       body: [
         `<div style='font-family: sans-serif; font-size: 16px; color: #111;'>`,
-        `<p>Tipo do feedback: ${request.type}</p>`,
-        `<p>Comentário: ${request.comment}</p>`,
+        `<p>Tipo do feedback: ${type}</p>`,
+        `<p>Comentário: ${comment}</p>`,
         `</div>`,
       ].join('\n'),
     });
